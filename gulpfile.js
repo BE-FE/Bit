2188563704d26cbf2f3f9f3a55c90f54ae22df44
shinate <shine.wangrs@gmail.com>
2015-03-11 16:12:01 +0800

    confirm direct

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,9 @@ var uglify = require('gulp-uglify');
 var clean = require('gulp-clean');
 
 var CONF = {
-	'src' : './src/',
-	'dest' : './release/'
+    'src' : './src/',
+    'dest' : './release/',
+    'test' : './test/'
 };
 
 gulp.task('default', ['release']);
@@ -17,29 +18,25 @@ gulp.task('release', ['js']);
 
 gulp.task('reflush', ['release']);
 
-gulp.task('server', function() {
-	connect.server({
-		port : 8000,
-		root : CONF.src,
-		livereload : true
-	});
+gulp.task('test', function() {
+    connect.server({
+        port : 8000,
+        root : CONF.test,
+        livereload : true
+    });
 });
 
 gulp.task('clean', function() {
-	return gulp.src(CONF.dest)
-		.pipe(clean());
+    return gulp.src(CONF.dest).pipe(clean());
 });
 
 gulp.task('js', function() {
-	// ;off
-	gulp.src(CONF.src + 'js/Bit.js')
+    // ;off
+	gulp.src(CONF.src + 'Bit.js')
+	    .pipe(gulp.dest(CONF.dest))
 		.pipe(uglify())
 		.pipe(rename('Bit.min.js'))
-		.pipe(gulp.dest(CONF.dest + 'js/'));
-		
-    gulp.src(CONF.src + 'js/Bit.js')
-        .pipe(rename('Bit.js'))
-        .pipe(gulp.dest(CONF.dest + 'js/'));
+		.pipe(gulp.dest(CONF.dest));
 	// ;on
 });
 

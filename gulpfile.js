36b811e7324f42f29081e1adf24c5ad46cff9d4b
unknown <wangrunshi@.internal.baidu.com>
2015-03-18 16:24:54 +0800

    reset direct

--- gulpfile.js
+++ gulpfile.js
@@ -6,30 +6,18 @@ var clean = require('gulp-clean');
 
 var CONF = {
     'src' : './src/',
-    'dest' : './release/',
-    'test' : './test/'
+    'dest' : './release/'
 };
 
 gulp.task('default', ['release']);
 
-gulp.task('server', ['connect']);
-
 gulp.task('release', ['js']);
 
-gulp.task('reflush', ['release']);
-
-gulp.task('test', ['test-watch'], function() {
-
-    gulp.watch([CONF.src + '**/*'], ['test-watch']);
-    
+gulp.task('server', function() {
     connect.server({
         port : 8000,
-        root : CONF.test
+        root : CONF.src
     });
-});
-
-gulp.task('test-watch', function() {
-    return gulp.src(CONF.src + 'Bit.js').pipe(gulp.dest(CONF.test));
 });
 
 gulp.task('clean', function() {
